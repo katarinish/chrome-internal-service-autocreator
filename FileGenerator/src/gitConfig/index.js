e41e07a25a90f7cb4d6dfe821007a81709b4a9ac
Yekaterina Kovylina <katarinish@yandex-team.ru>
2020-03-03 20:43:44 +0700

    extracting global git config

---
+++ FileGenerator/src/gitConfig/index.js
@@ -0,0 +1,14 @@
+const gitconfig = require('git-config-path');
+const parse = require('parse-git-config');
+
+const getCurrentConfig = () => {
+    const globalGitConfigPath = gitconfig({type: 'global'});
+    const options = {cwd: '/', path: globalGitConfigPath};
+    const config = parse.sync(options) || {};
+
+    return config.user;
+};
+
+module.exports = {
+    getCurrentConfig,
+};
